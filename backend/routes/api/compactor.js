cee672feb66cc8c52305b63e7893ba37a6a8865f
blazefire710 <paul.soh.2020@sis.smu.edu.sg>
2022-06-29 12:52:58 +0800

    API Route POST Working

--- backend/routes/api/compactor.js
+++ backend/routes/api/compactor.js
@@ -7,10 +7,12 @@ const Compactor = require('../../models/compactor');
 
 // @route GET api/compactor/test
 // @description tests compactor route
+// !!WORKS!!//
 router.get('/test', (req, res) => res.send('compactor route testing successful'));
 
 // @route GET api/compactor
 // @description Get all compactor
+// !!WORKS!!//
 router.get('/', (req, res) => {
   Compactor.find()
     .then(compactor => res.json(compactor))
@@ -19,15 +21,18 @@ router.get('/', (req, res) => {
 
 // @route GET api/compactor/:id
 // @description Get single compactor by id
+// !!WORKS!!//
 router.get('/:id', (req, res) => {
   Compactor.findById(req.params.id)
     .then(compactor => res.json(compactor))
     .catch(err => res.status(404).json({ nocompactorfound: 'No compactor found' }));
 });
 
-// @route POST api/compactor
+// @route POST api/compactor/post
 // @description add/save compactor
-router.post('/', (req, res) => {
+// Old Testing version of Post without  proper handling of request post data
+
+router.post('/post', (req, res) => { // route is edited to /api/post for testing.
   // !! Working Here !! //
   console.log(req.body);
   Compactor.create(req.body)
@@ -35,8 +40,25 @@ router.post('/', (req, res) => {
     .catch(err => res.status(400).json({ error: 'Unable to add this compactor' }));
 });
 
+
+// @route POST api/compactor
+// @description add/save compactor 
+// !!WORKS!!//
+router.post('/', async (req, res) => {
+  const compactor = new Compactor({
+    username: req.body.username,
+    items: req.body.items
+  })
+  try {
+    const newCompactor = await compactor.save()
+    res.status(201).json(newCompactor)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
+
 // @route POST api/compactor/:id
-// @description Update compactor
+// @description Update compactor Old version
 router.put('/:id', (req, res) => {
   Compactor.findByIdAndUpdate(req.params.id, req.body)
     .then(compactor => res.json({ msg: 'Updated successfully' }))
