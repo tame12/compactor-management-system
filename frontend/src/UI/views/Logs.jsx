cddbff477926cf98403451662cbc45c3892a22c0
mxixqc <64306579+mxixqc@users.noreply.github.com>
2022-07-17 11:54:26 +0800

    Update Logs.jsx

--- frontend/src/UI/views/Logs.jsx
+++ frontend/src/UI/views/Logs.jsx
@@ -2,29 +2,61 @@ import React, { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 
 const Logs = () => {
-	const [backData, setbackData] = useState([{}]);
+	const [logsData, setbackData] = useState([{}]);
 	useEffect(() => {
-		fetch("/gapi")
+		fetch("http://localhost:8082/api/logging")
 			.then((response) => response.json())
 			.then((data) => setbackData(data));
 	}, []);
 	// I think the empty [] makes it render just 1x when the thing is loaded up.
 
-  // Getting Headers 
-  var headers = ``;
-	backData.values[7]?.map((key) => (headers += `<th key=${key}>${key}</th>`));
+	// Getting Headers
+	console.log("Here is the logs Data:", logsData);
 
-  // Getting Contents
+	//   Data Pre-Processing
+	var headers = ``;
+	const titles = [
+		'Date',
+		'Time',
+		'Compactor',
+		'Item',
+		'Stock In / Stock Out',
+		'Quantity',
+		'Person',
+
+	]
+	titles.map((header) => headers += `<th key=${header}>${header}</th>`)
+
+	// Getting Contents
 	var contents = ``;
-	for (var i = 8; i < backData.values.length; i++) {
-		var info = backData.values[i];
-		var row = `<tr>`;
-    for (var l = 0; l < info.length; l++){
-      row += `<td>${info[l]}</td>`;
-    }
-    row += `<tr>`;
-    contents += row
+	var prevDate = new Date(logsData[0].createdAt)
+	prevDate = prevDate.getDate() +'/'+ prevDate.getMonth() + "/"+ prevDate.getFullYear()
+
+	for (var i=0;i < logsData.length; i++){
+		
+		var date = new Date(logsData[i].createdAt)
+		var currDate = date.getDate() +'/'+ date.getMonth() + "/"+ date.getFullYear()
+		if (currDate != prevDate){
+			prevDate = currDate
+			contents += `<tr class="blank_row" style="background-color: #A9A9A9;"><td colspan=${titles.length}></td></tr>`
+		}
+		contents += `<tr>`
+		contents += `<td>`+ currDate + `</td>`
+		
+		
+
+		contents += `</tr>`
+		
 	}
+	// for (var i = 8; i < backData.values.length; i++) {
+	// 	var info = backData.values[i];
+	// 	var row = `<tr>`;
+	// for (var l = 0; l < info.length; l++){
+	//   row += `<td>${info[l]}</td>`;
+	// }
+	// row += `<tr>`;
+	// contents += row
+	// }
 
 	return (
 		<div>
