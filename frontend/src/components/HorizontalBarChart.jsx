bd818fb65e5e33884c9df12e7d0dfb71a52683a8
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-07-23 20:23:28 +0800

    dashboard gets data and renders dynamically

--- frontend/src/components/HorizontalBarChart.jsx
+++ frontend/src/components/HorizontalBarChart.jsx
@@ -33,7 +33,7 @@ const HorizontalBarChart = (props) => {
                             <Tooltip />
                             {/* <Legend /> */}
                             {/* <Bar dataKey="pv" fill="#8884d8" /> */}
-                            <Bar dataKey="uv" fill="#82ca9d" />
+                            <Bar dataKey="quantity" fill="#82ca9d" />
                         </BarChart>
                     </ResponsiveContainer>
                 </div>
